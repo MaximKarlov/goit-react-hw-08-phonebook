1470b5f49057c99deb892649307a293934aac11d
MaximKarlov <predator09071987@gmail.com>
2023-05-10 01:30:47 +0300

    add material on Login & Regist

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import Button from '@mui/material/Button';
 import LoginIcon from '@mui/icons-material/Login';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import CSS from '../pages/Home.module.css';
 import { logInUser } from '../redux/auth/authOperation';
 
@@ -9,6 +17,14 @@ const Login = () => {
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  console.log('Login', email);
+  console.log('pass', password);
+  const handleClickShowPassword = () => setShowPassword(show => !show);
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
 
   const handleChange = ({ target: { name, value } }) => {
     switch (name) {
@@ -39,14 +55,37 @@ const Login = () => {
       <div className={CSS.registration}>
         <div className={CSS.items}>
           <h2>Вхід</h2>
-          <label>
-            Пошта
-            <input name="email" value={email} className={CSS.Email} type="email" onChange={handleChange} />
-          </label>
-          <label>
-            Пароль
-            <input name="password" value={password} className={CSS.Password} type="password" onChange={handleChange} />
-          </label>
+          <div className={CSS.items_input}>
+            <TextField
+              onChange={handleChange}
+              id="demo-helper-text-aligned"
+              label="Email"
+              name="email"
+              className={CSS.item_input}
+            />
+            <FormControl variant="outlined" className={CSS.item_input}>
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                onChange={handleChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </div>
           <Button variant="outlined" startIcon={<LoginIcon />} className={CSS.home_btn} type="submit">
             Вхід
           </Button>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,18 +2,35 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import CSS from '../pages/Home.module.css';
 import Button from '@mui/material/Button';
+
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { registerUser } from '../redux/auth/authOperation';
 
 const Register = () => {
   const dispatch = useDispatch();
-  const [nickName, setNickName] = useState('');
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  console.log('Login', email);
+  console.log('pass', password);
+  const handleClickShowPassword = () => setShowPassword(show => !show);
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
 
   const handleChange = ({ target: { name, value } }) => {
     switch (name) {
       case 'name': {
-        setNickName(value);
+        setName(value);
         break;
       }
       case 'email': {
@@ -32,8 +49,8 @@ const Register = () => {
 
   const regUser = e => {
     e.preventDefault();
-    dispatch(registerUser({ nickName, email, password }));
-    setNickName('');
+    dispatch(registerUser({ name, email, password }));
+    setName('');
     setEmail('');
     setPassword('');
   };
@@ -43,18 +60,44 @@ const Register = () => {
       <div className={CSS.registration}>
         <div className={CSS.items}>
           <h2>Реєстрація</h2>
-          <label>
-            Имя
-            <input name="name" value={nickName} className={CSS.Name} type="text" onChange={handleChange} />
-          </label>
-          <label>
-            Пошта
-            <input name="email" value={email} className={CSS.Email} type="email" onChange={handleChange} />
-          </label>
-          <label>
-            Пароль
-            <input name="password" value={password} className={CSS.Password} type="password" onChange={handleChange} />
-          </label>
+          <div className={CSS.items_input}>
+            <TextField
+              onChange={handleChange}
+              id="demo-helper-text-name"
+              label="Name"
+              name="name"
+              className={CSS.item_input}
+            />
+            <TextField
+              onChange={handleChange}
+              id="demo-helper-text-email"
+              label="Email"
+              name="email"
+              className={CSS.item_input}
+            />
+            <FormControl variant="outlined" className={CSS.item_input}>
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                onChange={handleChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </div>
           <Button variant="outlined" className={CSS.home_btn} type="submit">
             Реєстрація
           </Button>
