5d7957933d7e2dac17e221946ee73163e1d5c78f
MaximKarlov <predator09071987@gmail.com>
2023-05-09 15:56:29 +0300

    add Contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import { Layout } from './Layout';
 const Home = lazy(() => import('../pages/Home'));
 const Register = lazy(() => import('../pages/Register'));
 const Login = lazy(() => import('../pages/Login'));
-const Contacts = lazy(() => import('../pages/сontacts'));
+const Contacts = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
   const dispatch = useDispatch();
